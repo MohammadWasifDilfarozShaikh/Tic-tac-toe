202a9b890e048c5006d3cd61a7eb888c74cc8096
shaikhwasif720@gmail.com <shaikhwasif720@gmail.com>
2024-07-14 06:20:12 +0530

    Initil commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TickTackToe from './component/TickTacToe'
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <TickTackToe/>
+    </div>
+  )
+}
+
+export default App
